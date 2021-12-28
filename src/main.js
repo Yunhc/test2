74650dd16e9febc8dde75f8b4fa548c0784c4746
Yunhc <yunhc@wavelink.co.kr>
2021-12-28 14:25:29 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import VueHtmlToPaper from 'vue-html-to-paper';
+
+// import VueHtmlToPaper from 'vue-html-to-paper';
 // import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 // Import Bootstrap an BootstrapVue CSS files (order is important)
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-// import axios from 'axios';
+import axios from 'axios';
 import print from 'vue3-print-nb';
 
 // import java.io.BufferedReader;
@@ -25,30 +27,37 @@ import print from 'vue3-print-nb';
 // import javax.print.SimpleDoc;
 
 
-const options = {
-  name: '_blank',
-  specs: [
-    'fullscreen=yes',
-    'titlebar=yes',
-    'scrollbars=yes'
-  ],
-  styles: [
-    'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
-    'https://unpkg.com/kidlat-css/css/kidlat.css'
-  ],
-  timeout: 1000, // default timeout before the print window appears
-  autoClose: true, // if false, the window will not close after printing
-  windowTitle: window.document.title // override the window title
-};
+// const options = {
+//   name: '_blank',
+//   specs: [
+//     'fullscreen=yes',
+//     'titlebar=yes',
+//     'scrollbars=yes'
+//   ],
+//   styles: [
+//     'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+//     'https://unpkg.com/kidlat-css/css/kidlat.css'
+//   ],
+//   timeout: 1000, // default timeout before the print window appears
+//   autoClose: true, // if false, the window will not close after printing
+//   windowTitle: window.document.title // override the window title
+// };
 
 const app = createApp(App);
+
+//전역변수 선언
+app.config.globalProperties.$axios = axios;
+app.config.globalProperties.$url_rest = 'http://192.168.134.8:8090';
+
 app.use(router);
 app.use(store);
 app.use(print);
 
 app.mount('#app');
 
+
+
 // app.use(BootstrapVue);
 // app.use(IconsPlugin);
 
-app.use(VueHtmlToPaper, options);
+// app.use(VueHtmlToPaper, options);
