e6f78657a965e172903e83df587142fab1fd1ea1
Yunhc <yunhc@wavelink.co.kr>
2021-12-28 14:25:46 +0900

    Create auth.service.js

---
+++ src/services/auth.service.js
@@ -0,0 +1,41 @@
+
+import $axios from 'axios';
+
+const API_URL = 'http://192.168.134.8:8090' + '/api/auth/';
+const lang = 'KR';
+
+class AuthService {
+  login(user) {
+    console.log("[auth.service] = login -- ", lang + '/' + user);
+    return $axios
+      .post(API_URL + 'signin', {
+        lang: lang,
+        userid: user.value.userid,
+        password: user.value.password,
+      })
+      .then(response => {
+
+        console.log("[auth.service] = response -- ", response.data);
+        console.log("[auth.service] = response -- ", response.data[0].username);
+
+        if (response.data[0].code == "OK") {
+          console.log("[auth.service] = response -- ", response.data[0].code);
+          localStorage.setItem('user', JSON.stringify(response.data));
+        }
+        return response.data;
+      });
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+  }
+
+  register(user) {
+    return $axios.post(API_URL + 'signup', {
+      username: user.username,
+      password: user.password
+    });
+  }
+}
+
+export default new AuthService();
