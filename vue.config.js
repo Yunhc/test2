2faa9cc2c30b945b561fa577cf0093c4701e1e1e
Yunhc <yunhc@wavelink.co.kr>
2021-12-29 11:36:11 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,16 @@
 module.exports = {
     devServer: {
-        overlay: false,
-        proxy:{
-            'api':{
-                target: 'http://localhost:8001',
-                ws:true,
-                changeOrigin:true
-            },
-        }
+        // 사용자 정의 환경 변수에서 VUE_APP_PORT가 있으면 사용하고
+        // 없으면 3000 포트로 개발서버를 실행합니다.
+        port: process.env.VUE_APP_PORT || 8080,
+        // overlay: false,
+        // proxy:{
+        //     'api':{
+        //         target: 'http://localhost:8001',
+        //         ws:true,
+        //         changeOrigin:true
+        //     },
+        // },
     },
     // css: {
     //     loaderOptions: {
