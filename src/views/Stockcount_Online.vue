a7073361ecc06e054d0115d3518a27a2369d6d0c
Yunhc <yunhc@wavelink.co.kr>
2022-01-06 11:23:13 +0900

    Update Stockcount_Online.vue

--- src/views/Stockcount_Online.vue
+++ src/views/Stockcount_Online.vue
@@ -1,37 +1,43 @@
 <template>
-  <div class="stc">
-    <div class="stc_on">
-      <form class="d-flex" :style="{height:'37px'}" >
-        <div class="input-group mb-3">
-          <span class="input-group-text btn-sm" id="basic-addon1"
-            :style="{width:'80px'}"
-          >Stor Loc</span>
-          <select class="form-select btn-sm" aria-label="Default select example" v-model="user_param.useflag">
-            <option disabled value="">select location</option>
-            <option v-for="(d, i) in options" :key="i" :value="d.id">{{ d.name }}</option>
-          </select>
-        </div>
-      </form>
-      <form class="d-flex" :style="{height:'37px', margin:'-5px 0px 0px 0px'}" >
-        <div class="input-group mb-3">
-          <span class="input-group-text btn-sm" id="basic-addon1"
-            :style="{width:'80px'}">SC Type
-          </span>
-          <input type="text" class="form-control btn-sm" placeholder="SC Type" aria-label="SC Type" aria-describedby="basic-addon1" v-model="user_param.sctype">
-        </div>
-      </form>
-      <form class="d-flex" :style="{height:'37px', margin:'-5px 0px 0px 0px'}" >
-        <div class="input-group mb-3">
-          <span class="input-group-text btn-sm" id="basic-addon1"
-            :style="{width:'80px'}"
-          >Row No</span>
-          <input type="text" class="form-control btn-sm" placeholder="Row No" aria-label="Row No" aria-describedby="basic-addon1" v-model="user_param.rowno">
-        </div>
-      </form>
+  <div class="stc_on">
+    <div class="stc_on_search">
+      <div class="input-group mb-3" :style="{ margin:'0px 0px 0px 0px'}">
+        <span class="input-group-text btn-sm" id="basic-addon1"
+          :style="{width:'80px'}"
+        >Stor Loc</span>
+        <select class="form-select btn-sm" aria-label="Default select example"
+          id="stor_loc"
+          ref="stor_loc"
+          @change ='keyupenter'
+          v-model="req_param.stor_loc">
+          <option disabled value="">Select location</option>
+          <option v-for="(d, i) in options" :key="i" :value="d.id">{{ d.name }}</option>
+        </select>
+      </div>
+      <div class="input-group mb-3" :style="{ margin:'-15px 0px 0px 0px'}">
+        <span class="input-group-text btn-sm" id="basic-addon1"
+          :style="{width:'80px'}">SC Type
+        </span>
+        <input type="text" class="form-control btn-sm" placeholder="SC Type" aria-label="SC Type" aria-describedby="basic-addon1"
+          id="sctype"
+          ref="sctype"
+          @keyup.enter ='keyupenter'
+          v-model="req_param.sctype">
+      </div>
+      <div class="input-group mb-3" :style="{ margin:'-15px 0px 0px 0px'}">
+        <span class="input-group-text btn-sm" id="basic-addon1"
+          :style="{width:'80px'}"
+        >Row No</span>
+        <input type="text" class="form-control btn-sm" placeholder="Row No" aria-label="Row No" aria-describedby="basic-addon1"
+          id="rowno"
+          ref="rowno"
+          @keyup.enter ='keyupenter'
+          v-model="req_param.rowno">
+      </div>
     </div>
     <div class="stc_on_grid1"
       :style="{
-        'height': `calc(${window_height - 119 - 98 - 150}px)`
+        'height': `calc(${window_height - 119 - 98 - 123}px)`
       }"
     >
       <ag-grid-vue
@@ -46,35 +52,38 @@
       </ag-grid-vue>
     </div>
     <div class= "stc_on_save">
-      <form class="d-flex" :style="{height:'37px', margin:'2px 0px 0px 0px'}" >
-        <div class="input-group mb-3">
-          <input type="text" class="form-control btn-sm" placeholder="Scan barcode" aria-label="Scan barcode" aria-describedby="basic-addon1"
-            :style="{background:'Yellow'}"
-            v-model="user_param.rowno">
-        </div>
-      </form>
-      <form class="d-flex" :style="{height:'37px', margin:'-5px 0px 0px 0px'}" >
-        <div class="input-group mb-3">
-          <label for="exampleFormControlInput1" class="form-label">
-            {{user_param.rowno}}
-          </label>
-        </div>
-      </form>
-      <div align="right">
-        <button class="btn btn-outline-success btn-sm" type="button" :style="{ margin:'2px 10px 0px 0px', width:'70px'}"
-        @click='deleteClick'>Scan</button>
-        <button class="btn btn-outline-success btn-sm" type="button" :style="{ margin:'2px 5px 0px 0px', width:'70px'}"
-        @click='saveClick'>Close</button>
+      <div class="input-group mb-3" :style="{height:'30px', margin:'2px 0px 0px 0px'}">
+        <input type="text" class="form-control btn-sm" placeholder="Scan barcode" aria-label="Scan barcode" aria-describedby="basic-addon1"
+          id="scan"
+          ref="scan"
+          @keyup.enter ='keyupenter'
+          data-ref="InputContent" inputmode="none"
+          v-model="req_param.scan">
+      </div>
+      <div class="input-group mb-3"
+        :style="{height:'50px', margin:'-16px 0px 0px 0px', background:'gainsboro'}">
+        <p :style="{margin:'2px 0px 0px 0px', background:'transparent'}">
+          Msg:{{msg}}
+        </p>
+      </div>
+      <div align="right" :style="{height:'40px', margin:'-17px 0px 0px 0px'}">
+        <button class="btn btn-outline-success btn-sm" type="button" :style="{ margin:'5px 10px 0px 0px', width:'70px'}"
+        @click='scanClick'>Scan</button>
+        <button class="btn btn-outline-success btn-sm" type="button" :style="{ margin:'5px 5px 0px 0px', width:'70px'}"
+        @click='closeClick'>Close</button>
       </div>
     </div>
   </div>
 </template>
 <script>
   import $axios from 'axios';
-  import { reactive, ref, onMounted, onUnmounted} from 'vue'
+  import { reactive, ref, onBeforeMount, onMounted, onUnmounted } from 'vue'
   import 'ag-grid-community/dist/styles/ag-grid.css';
   import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
   import {AgGridVue} from 'ag-grid-vue3'
+  import { useStore } from 'vuex';
+  import { getdata } from '@/helper/filter.js';
+  import { searchSelectBox } from '@/helper/sql.js';
 
   export default {
     name:'stockcount_online',
@@ -82,56 +91,33 @@
       AgGridVue,
     },
     setup(){
-      const $url_rest = process.env.VUE_APP_SERVER_URL;
-      let rowData = reactive([]);
-      let url = ref($url_rest);
-      let gridApi = ref(null);
-      let columnApi = ref(null);
-      let user_param = reactive({sctype:"", rowno:"", stor_loc:""});
-      let columnDefs= reactive([
-          {headerName: '선택', field: 'sel', width: 50, cellStyle: {textAlign: "center"},
-            headerCheckboxSelection: true, checkboxSelection: true, pinned: 'left'},
-          {headerName: '사용자ID', field: 'sctype', width: 100, sortable: true, pinned: 'left'},
-          {headerName: '사용자명', field: 'rowno', width: 100, pinned: 'left'},
-          {headerName: '패스워드', field: 'userpwd', hide: true, cellStyle: { color: 'red', textAlign: "left", backgroundColor: "white" }},
-          {headerName: '플랜드', field: 'plantcd', width: 250, sortable: true, filter: true},
-          {headerName: '작업장', field: 'wccode', hide: true},
-          {headerName: '창고', field: 'warehouse'},
-          {headerName: '조회그룹', field: 'auth'},
-          {headerName: 'Role', field: 'role'},
-          {headerName: 'Use Role', field: 'use_role'},
-          {headerName: '사용유무', field: 'stor_loc', cellStyle: {textAlign: "center"},
-            editable: true,
-            cellEditor: 'agSelectCellEditor',
-            cellRenderer: stor_locCellRenderer,
-            keyCreator: (stor_loc) => {
-              return stor_loc.name;
-            },
-            cellEditorParams: function(param){
-              var selectedPlant = getdata(param.data.plantcd);
-              console.log("[selectedPlant]", selectedPlant );
-              if (selectedPlant == 'K143'){
-                return{
-                  values: ['Y', 'N']
-                };
-              }
-              else{
-                return{
-                  values: ['Y', 'N']
-                };
-              }
-            },
-          },
-          {headerName: '지게차 번호', field: 'forklift'},
-          {headerName: '비고', field: 'etc'},
-          {headerName: '수정자', field: 'upduser'},
-          {headerName: '수정일', field: 'upddate', cellStyle: {textAlign: "center"},},
-      ]);
-      const stor_locCellRenderer = (params) => {
-        console.log("stor_locCellRenderer--");
-        params.value.name;
-      };
+      let url = ref(process.env.VUE_APP_SERVER_URL);
+      let window_width = ref(window.innerWidth);
+      let window_height = ref(window.innerHeight);
 
+      const store = useStore();	//스토어호출
+      let options = reactive([]);
+
+      let rowData = reactive([]);
+      let gridApi = ref(null);
+      //focus 이동을 위한 변수
+      let sctype = ref(null);
+      let rowno = ref(null);
+      let stoc_loc = ref(null);
+      let scan = ref(null);
+      //데이터 바인딩
+      let req_param = reactive({stor_loc:"", sctype:"", rowno:"", scan:""});
+      let msg = ref(null);
+
+      let columnDefs= reactive([
+        {headerName: 'Barcode', field: 'barno', width: 80, sortable: true, pinned: 'left'},
+        {headerName: 'Material', field: 'matnr', width: 100, sortable: true, pinned: 'left'},
+        {headerName: 'Quantity', field: 'qty', width: 100, pinned: 'left'},
+        {headerName: 'Plant', field: 'werks', width: 100, cellStyle: { color: 'red', textAlign: "left", backgroundColor: "white" }},
+        {headerName: 'S/L', field: 'lgort', width: 250, sortable: true, filter: true},
+        {headerName: 'Row NO', field: 'rowno', width: 250},
+        {headerName: 'Material Description', field: 'desc'},
+      ]);
       var gridOptions = {
         defaultColDef: {
           width: 150,
@@ -158,15 +144,18 @@
         },
       };
 
-      let window_width = ref(window.innerWidth);
-      let window_height = ref(window.innerHeight);
+      onBeforeMount(()=>{
+        console.log("[Stockcount_Online] = ", "onBeforeMount--");
+        initSelectBox();
+      });
 
       onMounted(() => {
-        console.log("[UserManagement] = ", "onMounted--");
+        console.log("[Stockcount_Online] = ", "onMounted--");
         window.addEventListener('resize', handleResize);
       });
+
       onUnmounted(() =>{
-        console.log("[UserManagement] = onUnmounted -- ");
+        console.log("[Stockcount_Online] = onUnmounted -- ");
       });
 
       function handleResize() {
@@ -174,10 +163,6 @@
         window_height.value = window.innerHeight;
       }
 
-      const onGridReady = params => {
-        gridApi = params.api;
-        columnApi.value = params.columnApi;
-      };
       const getSelectedRows = () => {
         const selectedNodes = gridApi.value.getSelectedNodes();
         const selectedData = selectedNodes.map( node => node.data );
@@ -188,14 +173,14 @@
       function searchClick_post() {
         let urlPost = url.value + '/dw_userList_p';
 
-        console.log("[user_param.sctype]", user_param.sctype);
-        console.log("[user_param.rowno]", user_param.rowno);
-        console.log("[user_param.stor_loc]", user_param.stor_loc);
+        console.log("[req_param.sctype]", req_param.sctype);
+        console.log("[req_param.rowno]", req_param.rowno);
+        console.log("[req_param.stor_loc]", req_param.stor_loc);
 
         $axios.post(urlPost, {
-          sctype: user_param.sctype,
-          rowno: user_param.rowno,
-          stor_loc: user_param.stor_loc,
+          sctype: req_param.sctype,
+          rowno: req_param.rowno,
+          stor_loc: req_param.stor_loc,
         })
         .then((res) => {
           console.log("[response data]", res.data);
@@ -208,88 +193,81 @@
         })
       }
 
-      function createNewRowData() {
-        var newData = {
-          sctype:"",
-          rowno:"",
-          stor_loc:""
-        };
-        return newData;
-      }
-
-      function addClick() {
-        var newItem = createNewRowData();
-        // 맨밑에 추가
-        // var res = gridOptions.api.updateRowData({add: [newItem]});
-        // rowData.add(res);
-
-        // 특정위치에 추가
-        var res = gridOptions.api.updateRowData({add: [newItem],addIndex:0});
-        rowData.add(res).addIndex(0);
-        //printResult(res);
-      }
-
-      // function saveUser2(sType) {
-      //   var msg = "";
-      //   var selectedData = gridOptions.api.getSelectedRows();
-      //   console.log("[checked row]", selectedData);
-      //   let urlPost = url.value + '/dw_usersaveList_p_j';
-      //   $axios.post(urlPost, {
-      //       lang:       "KR",
-      //       type:       sType,
-      //       upduser:    "ADMIN",
-      //       data:       selectedData,
-      //     },
-      //   )
-      //   .then((res) => {
-      //     console.log(`status code: ${res.status}`);
-      //     console.log("[response data]", res.data);
-
-      //     if (res.data[0].status == "OK"){
-      //       msg = res.data[0].msg;
-      //       alert(msg);
-      //     }
-      //     else{
-      //       msg = res.data[0].msg;
-      //       alert(msg);
-      //     }
-      //   })
-      //   .catch(err => {
-      //     alert(err);
-      //     console.error(err)
-      //   });
-      // }
-      // 스트링 처리를 한다.
-      function getdata (data){
-        let rtn = "";
-        if (data != null){
-          var npos1 = data.indexOf("[",0);
-          var npos2 = data.indexOf("]",0);
-          if (npos1 > -1 && npos2 > -1){
-            rtn = data.substring(npos1 + 1, npos2 )
+      async function initSelectBox() {
+        let req_param = reactive(
+          {	lang:"KR",
+            userid:store.state.auth.user[0].userid,
+            plantcd:getdata(store.state.auth.user[0].plantcd),
+            type1:"LGORT_MWMS",
+            type2:"",
+            type3:"",
+            type4:"",
+            space:"Y",
           }
-          else{
-            rtn = data;
+        );
+        let res = reactive([]);
+        res = await searchSelectBox(req_param);
+        // console.log("[Stockcount_offline/initSelectBox] = res -- ", res);
+
+        if(res.data.length > 0){
+          options.splice(0, options.length);
+          for(var i=0; i<res.data.length; i++){
+              options.push({id:res.data[i].id, name:res.data[i].name});
           }
         }
-        return rtn;
+        console.log("[Stockcount_offline/initSelectBox] = options data -- ", options);
       }
+
+      function keyupenter(e){
+        if (e.target.id == "stor_loc"){
+          sctype.value.focus();
+        }
+        else if (e.target.id == "sctype"){
+          rowno.value.focus();
+        }
+        else if (e.target.id == "rowno"){
+          // console.log("[Stockcount_Online/keyupenter] = rowno -- ");
+          scan.value.focus();
+        }
+        else if (e.target.id == "scan"){
+          console.log(req_param.scan);
+          var txtscan = document.getElementById("scan");
+          txtscan.setAttribute('inputmode','none');
+          console.log(txtscan.inputMode);
+        }
+        // if(e.which == 13){
+        //   console.log(text)
+        // }
+      }
+
+      function scanClick() {
+        console.log(req_param.scan);
+        var txtscan = document.getElementById("scan");
+        txtscan.setAttribute('inputmode','numeric');
+        console.log(txtscan.inputMode);
+
+        scan.value.focus();
+      }
+
       return {
-        sctype: "",
-        rowno: "",
-        stor_loc: "",
+        window_width,
+        window_height,
+        stoc_loc,
+        sctype,
+        rowno,
+        scan,
+        req_param,
+        msg,
         scrollPostion : 0,
         defaultColGroupDef: null,
         columnTypes: null,
+        options,
         rowData,
-        onGridReady,
         gridOptions,
         getSelectedRows,
         searchClick_post,
-        user_param,
-        addClick,
-        window_width,
-        window_height,
+        keyupenter,
+        scanClick,
       };
     },
   }
@@ -298,7 +276,7 @@
   @import "~ag-grid-community/dist/styles/ag-grid.css";
   @import "~ag-grid-community/dist/styles/ag-theme-alpine.css";
 
-	.stc {
+	.stc_on {
 		font-family: Avenir, Helvetica, Arial, sans-serif;
 		-webkit-font-smoothing: antialiased;
 		-moz-osx-font-smoothing: grayscale;
@@ -306,8 +284,14 @@
 		color: #2c3e50;
 		width:100%;
     height:100%;
+    // select:focus {
+    //   background: yellow;
+    // }
+    input:focus {
+      background: yellow;
+    }
 	}
-  .stc_on {
+  .stc_on_search {
     height : 98px;
     margin : 0px 5px 0px 5px;
   }
@@ -316,7 +300,9 @@
     overflow: auto;
   }
   .stc_on_save {
-    height : 148px;
-    margin : 0px 50px 0px 5px;
+    // background: gainsboro;
+    height : 123px;
+    margin : 0px 5px 0px 5px;
+    overflow-x: auto;
   }
 </style>
