120e299631a56b54594cebcdf997bc089e2d05a0
Yunhc <yunhc@wavelink.co.kr>
2022-01-07 14:26:08 +0900

    Update Home2.vue

--- src/views/Home2.vue
+++ src/views/Home2.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="home" :style="{ height:'800px'}">
+  <div class="home"
+    :style="{
+      'height': `calc(${window_height - 110}px)`
+    }"
+  >
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue3.js App"/>
   </div>
@@ -8,11 +12,36 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import { ref, onMounted, onUnmounted } from 'vue'
 
 export default {
   name: 'home2',
   components: {
     HelloWorld
+  },
+  setup(){
+    let window_width = ref(window.innerWidth);
+    let window_height = ref(window.innerHeight);
+
+    onMounted(() => {
+      console.log("[Stockcount_Online] = ", "onMounted--");
+      window.addEventListener('resize', handleResize);
+    });
+
+    onUnmounted(() =>{
+      console.log("[Stockcount_Online] = onUnmounted -- ");
+      window.removeEventListener('resize', handleResize);
+    });
+
+    function handleResize() {
+      window_width.value = window.innerWidth;
+      window_height.value = window.innerHeight;
+    }
+
+    return{
+      window_width,
+      window_height,
+    };
   }
 }
 </script>
