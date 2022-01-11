f3dd6693c187f664c0ce291bad39a1f39975a312
Yunhc <yunhc@wavelink.co.kr>
2022-01-11 18:09:47 +0900

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,12 @@
 // import { createStore, Store } from 'vuex';
 import { createStore } from 'vuex';
 import { auth } from './modules/auth';
+import { saveid } from './modules/saveid';
 
 
 export default createStore({
   modules: {
     auth,
+    saveid,
   },
 });
