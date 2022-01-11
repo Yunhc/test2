a50207b61b947f4bc7a08d1d519294bb4f8e0957
Yunhc <yunhc@wavelink.co.kr>
2022-01-11 18:09:56 +0900

    Update Home2.vue

--- src/views/Home2.vue
+++ src/views/Home2.vue
@@ -24,12 +24,12 @@ export default {
     let window_height = ref(window.innerHeight);
 
     onMounted(() => {
-      console.log("[Stockcount_Online] = ", "onMounted--");
+      console.log("[Home2] = ", "onMounted--");
       window.addEventListener('resize', handleResize);
     });
 
     onUnmounted(() =>{
-      console.log("[Stockcount_Online] = onUnmounted -- ");
+      console.log("[Home2] = onUnmounted -- ");
       window.removeEventListener('resize', handleResize);
     });
 
