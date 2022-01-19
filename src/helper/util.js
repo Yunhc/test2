9809b5ff0a03ebcc7a585b9d23b70449f5abbb5f
Yunhc <yunhc@wavelink.co.kr>
2022-01-19 16:02:44 +0900

    Create util.js

---
+++ src/helper/util.js
@@ -0,0 +1,13 @@
+import OK from "@/assets/sound/OK.mp3";
+
+// sound play
+export function PlaySound(type){
+	console.log("PlaySound : type -- ", type);
+
+	var sound = "";
+	if (type == "OK"){
+		sound = OK;
+	}
+	var audio = new Audio(sound);
+	audio.play();
+}
