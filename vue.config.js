5f977263bd3c2c94d1e310cfe03ee0444ef7ff27
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-01-27 13:09:17 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -14,14 +14,13 @@ module.exports = {
         //     },
         // },
     },
-    // css: {
-    //     loaderOptions: {
-    //       scss: {
-    //         prependData: `
-    //           @import "@/assets/style/_mixins.scss";
-    //           @import "@/assets/style/_variables.scss";
-    //         `
-    //         }
-    //     }
-    // }
+    css: {
+        loaderOptions: {
+          scss: {
+            prependData: `
+              @import "@/assets/style/variables.scss";
+            `
+            }
+        }
+    }
 }
