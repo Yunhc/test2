7bd093ed0e7fff266e96964c9fcf320850fa0eac
Yunhc <yunhc@wavelink.co.kr>
2022-02-11 18:15:11 +0900

    Update QRCode.vue

--- src/views/QRCode.vue
+++ src/views/QRCode.vue
@@ -1,22 +1,41 @@
 <template>
-	<div :style="{ height:'630px'}">
-		<p>QR code 생성</p>
-			<div class="input-group">
-				<span class="input-group-text" id="inputGroup-sizing-default">QR Barcode Data 입력</span>
-				<input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"
-					v-model="qr_values">
-				<button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04" @click="QRCreate">바코드 생성</button>
-			</div>
-		<div :style="{margin:'20px 0px 50px 0px'}">
-			<qr-code
-				align="center"
-				v-bind:text="qrcode_1_text"
-				size=200
-				error-level="H"
-				@qrCode="getLink($event)"
-			></qr-code>
+	<div class="window-main">
+		<div :style="{padding:'10px 0px 0px 0px'}">
+			<p>QR code 생성</p>
 		</div>
-		<p>1차원 barcode 생성</p>
+		<div class="input-group">
+			<span class="input-group-text" id="inputGroup-sizing-default">QR Barcode Data 입력</span>
+			<input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"
+				v-model="qr_values">
+			<button class="btn btn-outline-secondary" type="button" id="inputGroupFileAddon04" @click="QRCreate">바코드 생성</button>
+		</div>
+		<div :style="{width:'100%', height:'250px', background:'transparent', border:'1px solid transparent'}">
+			<div class="left-box-50" :style="{margin:'20px 0px 20px 0px'}">
+				<qr-code
+					align="center"
+					v-bind:text="qrcode_1_text"
+					size=200
+					error-level="H"
+					@qrCode="getLink($event)"
+				></qr-code>
+			</div>
+			<div class="right-box-50" :style="{margin:'20px 0px 20px 0px', border:'1px solid transparent'}">
+				<div id="bar1" class="barcodeStyle">
+					<ejs-datamatrixgenerator
+						id="barcode"
+						ref="datamatrixControl"
+						:width="width"
+						:height="height"
+						:displayText="displaytext"
+						:value="value"
+						:mode="mode"
+					></ejs-datamatrixgenerator>
+				</div>
+			</div>
+		</div>
+		<div :style="{margin:'10px 0px 0px 0px', border:'1px solid transparent'}">
+			<p>1차원 barcode 생성</p>
+		</div>
 		<div>
 			<div class="input-group">
 				<span class="input-group-text" id="inputGroup-sizing-default">Barcode Data 입력</span>
@@ -31,32 +50,75 @@
 <script>
 import VueQRCodeComponent from 'vue-qrcode-component';
 import JsBarcode from 'jsbarcode';
+import { DataMatrixGeneratorComponent } from '@syncfusion/ej2-vue-barcode-generator';
+import { DataMatrixGenerator } from '@syncfusion/ej2-vue-barcode-generator';
+// import { BarcodeGeneratorComponent, QRCodeGeneratorComponent, DataMatrixGeneratorComponent } from '@syncfusion/ej2-vue-barcode-generator';
 
 export default {
 	name: 'qrcode',
 	components:{
     'qr-code': VueQRCodeComponent,
+		'ejs-datamatrixgenerator': DataMatrixGeneratorComponent,
   },
 	data(){
 		return{
 			qr_values : 'https://www.naver.com',
 			qrcode_1_text : 'https://www.naver.com',
 			barcodeValue: '1234567890128',
+
+			width: "100px",
+			height: "150px",
+			type: "DataMatrix",
+			value: "wavelink-0123456789",
+			mode: "SVG",
+			displaytext: { text: 'DataMatrix', visibility:false}
 		};
 	},
 	methods : {
 		QRCreate(){
 			this.qrcode_1_text = this.qr_values;
+			this.value = this.qr_values;
 		},
 		BarCreate(){
 			JsBarcode("#barcode")
 			.options({font: "OCR-B"}) // Will affect all barcodes
 			.CODE128(this.barcodeValue, {height: 85, textPosition: "bottom", fontSize: 16, marginTop: 20})
 			.render();
+		},
+
+		DataMatrixCreate(){
+			let barcode = new DataMatrixGenerator({
+				width: '100px',
+				height: '200px',
+				type: "DataMatrix",
+				value: "wavelink-123456789",
+				mode: "SVG",
+				displaytext: { text: 'DataMatrix', visibility:false}
+			});
+			barcode.appendTo('#bar1');
+
+			// this.barcode.width = '100px';
+			// this.barcode.height= '150px';
+			// this.barcode.type= "DataMatrix";
+			// this.barcode.value= "wavelink-123456789";
+			// this.barcode.mode= "SVG";
+			// this.barcode.displaytext= { text: 'DataMatrix', visiblilit:false};
 		}
 	},
+	Mounted(){
+	},
 	created(){
-		console.log("[QRCode] = create -- ")
+		// console.log("[QRCode] = create -- ")
 	}
 }
 </script>
+<style>
+.barcodeStyle {
+	height: 200px;
+	width: 250px;
+	/* padding-left: 1%;
+	padding-top: 1%; */
+
+	/* border:1px solid red; */
+}
+</style>
