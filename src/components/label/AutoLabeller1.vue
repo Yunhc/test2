dabcafb2d27b1e07d11692b41b1c8725a0486e7f
Yunhc <yunhc@wavelink.co.kr>
2022-02-17 11:46:30 +0900

    Update AutoLabeller1.vue

--- src/components/label/AutoLabeller1.vue
+++ src/components/label/AutoLabeller1.vue
@@ -1,9 +1,9 @@
 <template>
 	<div id="print_obj"
-		class="print-autolabeller"
+		class="print-autolabeller1"
 	>
 		<div class="print-al-content">
-			<div class="print-line"
+			<!-- <div class="print-line"
 				:style="{ height:'257px', margin:'0px 0px 0px 100px'}">
 			</div>
 			<div class="print-line"
@@ -11,13 +11,14 @@
 			</div>
 			<div class="print-line"
 				:style="{ height:'121px', margin:'53px 0px 0px 356px'}">
-			</div>
+			</div> -->
 
 			<div class="input-group mb-3 div-center " :style="{margin:'0px 0px 0px 0px', 'z-index': '21'}">
 				<label class="form-label"
 					:style="{width:'100px', padding:'15px 0px 0px 0px'}">
 					패&nbsp;&nbsp;&nbsp;턴&nbsp;&nbsp;&nbsp;명
 				</label>
+				<div class='print-line-v-1'></div>
 				<input type="text" class="form-control btn-sm ellipsis"
 					:style="{height:'50px', fontSize:'40px'}"
 					v-model="l_ptdesc"
@@ -28,14 +29,17 @@
 					:style="{width:'100px', padding:'5px 0px 0px 0px'}">
 					패 턴 코 드
 				</label>
+				<div class='print-line-v-2'></div>
 				<input type="text" class="form-control btn-sm"
 					:style="{height:'35px', fontSize:'24px'}"
 					v-model="l_ptcode"
 				>
+				<div class='print-line-v-2'></div>
 				<label class="form-label"
 					:style="{width:'110px', padding:'5px 0px 0px 0px'}">
 					LOT NO.
 				</label>
+				<div class='print-line-v-2'></div>
 				<input type="text" class="form-control btn-sm"
 					:style="{height:'35px', fontSize:'24px'}"
 					v-model="l_lotno"
@@ -46,14 +50,17 @@
 					:style="{width:'100px', padding:'5px 0px 0px 0px'}">
 					HDF생산일
 				</label>
+				<div class='print-line-v-2'></div>
 				<input type="text" class="form-control btn-sm"
 				:style="{height:'35px', fontSize:'24px'}"
 					v-model="l_date"
 				>
+				<div class='print-line-v-2'></div>
 				<label class="form-label"
 					:style="{width:'110px', padding:'5px 0px 0px 0px'}">
 					F.MFM생산일
 				</label>
+				<div class='print-line-v-2'></div>
 				<input type="text" class="form-control btn-sm"
 				:style="{height:'35px', fontSize:'24px'}"
 					v-model="l_date"
@@ -64,14 +71,17 @@
 					:style="{width:'100px', padding:'5px 0px 0px 0px'}">
 					가&nbsp;&nbsp;&nbsp;공&nbsp;&nbsp;&nbsp;일
 				</label>
+				<div class='print-line-v-2'></div>
 				<input type="text" class="form-control btn-sm"
 				:style="{height:'35px', fontSize:'24px'}"
 					v-model="l_date"
 				>
+				<div class='print-line-v-2'></div>
 				<label class="form-label"
 					:style="{width:'110px', padding:'5px 0px 0px 0px'}">
 					제 품 코 드
 				</label>
+				<div class='print-line-v-2'></div>
 				<input type="text" class="form-control btn-sm"
 				:style="{height:'35px', fontSize:'24px'}"
 					v-model="l_matnr"
@@ -82,6 +92,7 @@
 					:style="{width:'100px', margin:'5px 0px 0px 0px'}">
 					제&nbsp;&nbsp;&nbsp;품&nbsp;&nbsp;&nbsp;명
 				</label>
+				<div class='print-line-v-2'></div>
 				<input type="text" class="form-control btn-sm ellipsis"
 				:style="{height:'35px', fontSize:'24px'}"
 					v-model="l_maktx"
@@ -92,6 +103,7 @@
 					:style="{width:'100px', padding:'10px 0px 0px 0px'}">
 					수&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;량
 				</label>
+				<div class='print-line-v-3'></div>
 				<input type="text" class="form-control btn-sm"
 				:style="{height:'42px', fontSize:'32px'}"
 					v-model="l_qty"
@@ -100,7 +112,7 @@
 			<div class="div-close" :style="{height:'90px', margin:'-16px 0px 0px 0px', 'z-index': '27'}">
 				<div class="bardiv-normal al-left-box"
 					align='center'
-					:style="{height:'84px', margin:'0px 0px 0px 0px', border:'1px solid transparent'}"
+					:style="{height:'85px', margin:'0px 0px 0px 0px', border:'1px solid transparent'}"
 				>
 					<BarcodeGenerator
 					:value="l_barno"
@@ -112,13 +124,13 @@
 					:fontSize="12"
 					:elementTag="'svg'"/>
 				</div>
-				<div id="bar1" class="print-test-border al-right-box"
-					:style="{height:'84px', padding:'0px 0px 0px 15px', align:'center', border:'1px solid transparent'}">
+				<div id="bar1" class="al-right-box bardiv-dataMatrix"
+					:style="{height:'85px', align:'center'}">
 					<ejs-datamatrixgenerator
 						id="barcode"
 						ref="datamatrixControl"
-						width="60"
-						height="80"
+						width="85"
+						height="90"
 						:displayText="displaytext"
 						:value="l_barno"
 						mode="SVG"
