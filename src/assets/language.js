e63078c8902581833c315c009b6376354ab4b33d
Yunhc <yunhc@wavelink.co.kr>
2022-02-25 18:03:04 +0900

    Update language.js

--- src/assets/language.js
+++ src/assets/language.js
@@ -1,40 +1,22 @@
-var login = [
-	{
-		id: "userid",
-		kr: "사용자ID",
-		en: "User ID",
-	},
-	{
-		id: "password",
-		kr: "비밀번호",
-		en: "Password",
-	},
-	{
-		id: "chk",
-		kr: "비밀번호",
-		en: "Password",
-	},
-	{
-		id: "login",
-		kr: "로그인",
-		en: "Sing on",
-	},
-]
+var login = {
+	lblUserID: {kr: "사용자ID", en: "User ID"},
+	lblPassword:{kr: "비밀번호", en: "Password"},
+	lblCheckID:{kr: "사용자ID 저장", en: "Save User ID"},
+	lblLogin:{kr: "로그인", en: "Sing on"},
+}
 
-var mainheader = [
-	{
-			id: "login",
-			kr: "로그인",
-			en: "Sing on",
-	},
-	{
-		id: "login",
-		kr: "로그인",
-		en: "Sing on",
-	}
-]
+var mainheader = {
+	lblTitle:{kr: "Dongwha | WMS", en: "Dongwha | WMS"},
+	lblLogout:{kr: "로그아웃", en: "Sing off"},
+}
+
+var message = {
+	lblNotice:{kr: "알림", en: "Notice"},
+	lblLogoutMsg:{kr: "로그아웃 하시겠습니까?", en: "Do you want to sing off?"},
+}
 
 export default {
+	message,
 	login,
 	mainheader,
 }
