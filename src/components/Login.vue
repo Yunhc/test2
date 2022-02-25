d66fecd5b48aabd6b0eb783a91f31126de38bc2a
Yunhc <yunhc@wavelink.co.kr>
2022-02-25 11:43:10 +0900

    Update Login.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,15 +10,17 @@
 		<div :class="['login-right-box', window_width>800 ? {horizontal:true}:{vertical:true}]">
 			<div align="right" style="margin:5px 0px 0px 10px; color:black;">
 				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="radio" name="languageRadios"
-						v-model="rdoEN">
+					<input type="radio" name="languageRadios" value="EN"
+						v-model="rdoLang"
+						@change="radioChangeLang">
 					<label class="form-check-label">
 						English
 					</label>
 				</div>
 				<div class="form-check form-check-inline">
-					<input class="form-check-input" type="radio" name="languageRadios"
-						v-model="rdoKR">
+					<input type="radio" name="languageRadios" value="KR"
+						v-model="rdoLang"
+						@change="radioChangeLang">
 					<label class="form-check-label">
 						Korean
 					</label>
@@ -60,12 +62,12 @@
 					<div align="center" class="login-desc">
 						<p>Copyright (C) DONGWHA CO,.LTD. reserved</p>
 					</div>
-					<!-- <button class="btn btn-outline-success btn-sm" type="button"
+					<button class="btn btn-outline-success btn-sm" type="button"
 						:style="{ margin:'4px 10px 0px 0px', width:'70px'}"
 						@click='startClick'>Start</button>
 						<button class="btn btn-outline-success btn-sm" type="button"
 						:style="{ margin:'4px 10px 0px 0px', width:'70px'}"
-						@click='endClick'>End</button> -->
+						@click='endClick'>End</button>
 				</div>
 			</div>
 		</div>
@@ -85,8 +87,7 @@ export default {
 		let window_height = ref(window.innerHeight);
 
 		//라디오버튼
-		let rdoEN = ref(false);
-		let rdoKR = ref(true);
+		let rdoLang = ref(null);
 
 		// let name = ref("Login");
 		let user = ref({userid:"", password:""});
@@ -113,8 +114,13 @@ export default {
 			// console.log("[login] = saveid --", store.state.saveid.id);
 
 			window.addEventListener('resize', handleResize);
+			//언어 초기값 설정
+			if(store.state.setup.language)
+				rdoLang.value = store.state.setup.language;
+			else
+				rdoLang.value ="KR";
 
-			if(store.state.saveid.id != null){
+			if(store.state.saveid.id){
 				// console.log("[login] = saveid -- exist");
 				if(store.state.saveid.id[0].chk == true){
 					chkID.value = true;
@@ -131,10 +137,14 @@ export default {
 			window.removeEventListener('resize', handleResize);
 		});
 
-		function handleLogin() {
-			console.log("EN = ", rdoEN.value);
-			console.log("KR = ", rdoKR.value);
+		function radioChangeLang()
+		{
+			store.dispatch("setup/setLanguage", rdoLang.value);
+			// console.log("Language = ", rdoLang.value);
+			// console.log("storage Language = ", store.state.setup.language);
+		}
 
+		function handleLogin() {
 			if (user.value.userid && user.value.password) {
 				store.commit('loading/startLoading'); //진행표시 시작
 				setTimeout(function(){
@@ -211,8 +221,8 @@ export default {
 		return {
 			window_width,
 			window_height,
-			rdoEN,
-			rdoKR,
+			rdoLang,
+			radioChangeLang,
 			user,
 			userid,
 			password,
