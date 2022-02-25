5931805ecd0db4bda26c68e0a869cbd04c2168e9
Yunhc <yunhc@wavelink.co.kr>
2022-02-25 11:57:28 +0900

    Update saveid.js

--- src/store/modules/saveid.js
+++ src/store/modules/saveid.js
@@ -9,19 +9,16 @@ export const saveid = {
 	state: initialState,
 	actions: {
 		saveid({ commit }, id) {
-			// console.log("[actions - saveid] = ", id);
 			localStorage.setItem('id', JSON.stringify(id));
 			return commit('saveid', id);
 		},
 		deleteid({ commit }) {
-			// console.log("[deleteid] = id -- ");
 			localStorage.removeItem('id');
 			commit('deleteid');
 		},
 	},
 	mutations: {
 		saveid(state, id) {
-			// console.log("[mutations - saveid] = ", id);
 			state.status.saveflag = true;
 			state.id = id;
 		},
