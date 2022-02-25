db05b31d909f4c1158fafa3307536f699c49d064
Yunhc <yunhc@wavelink.co.kr>
2022-02-25 11:43:20 +0900

    Update loading.js

--- src/store/modules/loading.js
+++ src/store/modules/loading.js
@@ -6,30 +6,12 @@ const state = {
 export const loading =  {
 	namespaced: true,
 	state: state,
-	actions: {
-		startLoading({ commit }, LoadingStatus) {
-			localStorage.setItem('LoadingStatus', true);
-			console.log("[LoadingStatus] = ", LoadingStatus);
-			return LoadingStatus => {
-				commit('loading', LoadingStatus);
-				return Promise.resolve(LoadingStatus);
-			}
-		},
-		endLoading({ commit }, LoadingStatus) {
-			localStorage.setItem('LoadingStatus', false);
-			console.log("[LoadingStatus] = ", LoadingStatus);
-			return LoadingStatus => {
-				commit('loading', LoadingStatus);
-				return Promise.resolve(LoadingStatus);
-			}
-		},
-	},
 	mutations: {
 		startLoading(state){
 			state.LoadingStatus = true;
 		},
 		endLoading(state){
-				state.LoadingStatus = false;
+			state.LoadingStatus = false;
 		}
 	}
 }
