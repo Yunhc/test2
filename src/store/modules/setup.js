4537f545d923f3aa421aec88de565a3d411b07b1
Yunhc <yunhc@wavelink.co.kr>
2022-02-25 11:57:25 +0900

    Update setup.js

--- src/store/modules/setup.js
+++ src/store/modules/setup.js
@@ -1,34 +1,30 @@
 //환경변수 설정을 위한 전역 변수를 선언
 
-const lang = localStorage.getItem('language');
-const state = lang ? {language: lang} : {language: 'KR'};
+const lng = localStorage.getItem('language');
+const language = lng ? {language: lng} : {language: 'KR'};
 
 const port = localStorage.getItem('commport');
 const comm = port ? {commport: port} : {commport: 'COM1'};
 
 export const setup =  {
 	namespaced: true,
-	state: state,
+	language: language,
 	comm: comm,
 	actions: {
-		setLanguage({ commit }, lang) {
-			localStorage.setItem('language', lang);
-			// console.log("[actions - language] = ", lang);
-			return commit('setLanguage', lang);
+		setLanguage({ commit }, lng) {
+			localStorage.setItem('language', lng);
+			return commit('setLanguage', lng);
 		},
 		setCommport({ commit }, port) {
 			localStorage.setItem('commport', port);
-			// console.log("[actions - commport] = ", port);
 			return commit('setCommport', port);
 		},
 	},
 	mutations: {
-		setLanguage(state, lang){
-			// console.log("[mutations - language] = ", lang);
-			state.language= lang;
+		setLanguage(language, lng){
+			language.language= lng;
 		},
 		setCommport(comm, port){
-			// console.log("[mutations - commport] = ", port);
 			comm.commport= port;
 		},
 	}
