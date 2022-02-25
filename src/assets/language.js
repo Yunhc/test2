624832eeb1b7b752637fd7ff6d1f953514ba19fc
Yunhc <yunhc@wavelink.co.kr>
2022-02-25 14:56:50 +0900

    Create language.js

---
+++ src/assets/language.js
@@ -0,0 +1,40 @@
+var login = [
+	{
+		id: "userid",
+		kr: "사용자ID",
+		en: "User ID",
+	},
+	{
+		id: "password",
+		kr: "비밀번호",
+		en: "Password",
+	},
+	{
+		id: "chk",
+		kr: "비밀번호",
+		en: "Password",
+	},
+	{
+		id: "login",
+		kr: "로그인",
+		en: "Sing on",
+	},
+]
+
+var mainheader = [
+	{
+			id: "login",
+			kr: "로그인",
+			en: "Sing on",
+	},
+	{
+		id: "login",
+		kr: "로그인",
+		en: "Sing on",
+	}
+]
+
+export default {
+	login,
+	mainheader,
+}
