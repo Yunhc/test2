1a58ffbd98b9aa26e401a9a5f06e65209dcfc6c4
Yunhc <yunhc@wavelink.co.kr>
2022-02-28 10:39:54 +0900

    Update language.js

--- src/assets/language.js
+++ src/assets/language.js
@@ -10,6 +10,32 @@ var mainheader = {
 	lblLogout:{kr: "로그아웃", en: "Sing off"},
 }
 
+var menupopup = {
+	M000:{kr: "시스템 관리", en:"System Management"},
+	M100:{kr: "기준정보 관리", en:"Master Data"},
+	M200:{kr: "오더 관리", en:"Order Information System"},
+	M250:{kr: "라벨 발행이력", en:"Label Print History"},
+	M251:{kr: "자재라벨 발행", en:"Material Label print"},
+	M252:{kr: "제품/상품라벨 발행", en:"FGs & Goods Label print"},
+	M300:{kr: "입/출고 현황", en:"Goods Issue & receipt List"},
+	M500:{kr: "재고정보", en:"Stock Overview"},
+	M510:{kr: "로케이션 관리", en:"Location Management"},
+	M520:{kr: "재고실사", en:"Stock Count"},
+	M550:{kr: "품질 관리", en:"Quality Management"},
+	M600:{kr: "품질 검사 관리", en:"Quality Inspection Management"},
+	M900:{kr: "R/3 인터페이스", en:"R/3 SAP Interface"},
+	M950:{kr: "생산실적", en:"Production Management"},
+	M960:{kr: "강화마루 Tool 관리", en:""},
+	M980:{kr: "MOBILE", en:"MOBILe"},
+}
+
+var usermanagement = {
+	lblUserID: {kr: "사용자ID", en: "User ID"},
+	lblPassword:{kr: "비밀번호", en: "Password"},
+	lblCheckID:{kr: "사용자ID 저장", en: "Save User ID"},
+	lblLogin:{kr: "로그인", en: "Sing on"},
+}
+
 var message = {
 	lblNotice:{kr: "알림", en: "Notice"},
 	lblLogoutMsg:{kr: "로그아웃 하시겠습니까?", en: "Do you want to sing off?"},
@@ -19,4 +45,6 @@ export default {
 	message,
 	login,
 	mainheader,
+	menupopup,
+	usermanagement,
 }
