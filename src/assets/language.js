5945c8936c321b03a11f9914c907e813277475af
Yunhc <yunhc@wavelink.co.kr>
2022-02-28 14:04:07 +0900

    Update language.js

--- src/assets/language.js
+++ src/assets/language.js
@@ -31,9 +31,30 @@ var menupopup = {
 
 var usermanagement = {
 	lblUserID: {kr: "사용자ID", en: "User ID"},
-	lblPassword:{kr: "비밀번호", en: "Password"},
-	lblCheckID:{kr: "사용자ID 저장", en: "Save User ID"},
-	lblLogin:{kr: "로그인", en: "Sing on"},
+	lblUserName:{kr: "사용자명", en: "User Name"},
+	lblUseFlag:{kr: "사용유무", en: "Usage"},
+	lblSearch:{kr: "조회", en: "Display"},
+	lblAdd:{kr: "추가", en: "Add"},
+	lblDelete:{kr: "삭제", en: "Delete"},
+	lblSave:{kr: "저장", en: "Save"},
+	lblClose:{kr: "종료", en: "Close"},
+	grid1:[
+		{name:{kr: "선택", en: "Select"}},
+		{name:{kr: "사용자ID", en: "User ID"}},
+		{name:{kr: "사용자명", en: "User Name"}},
+		{name:{kr: "패스워드", en: "Password"}},
+		{name:{kr: "플랜트", en: "Plant"}},
+		{name:{kr: "작업장", en: "Workcenter"}},
+		{name:{kr: "창고", en: "Warehouse"}},
+		{name:{kr: "조회그룹", en: "Display group"}},
+		{name:{kr: "Role", en: "Role"}},
+		{name:{kr: "Use Role", en: "Use Role"}},
+		{name:{kr: "사용유무", en: "Usage"}},
+		{name:{kr: "지게차 번호", en: "Forklift NO."}},
+		{name:{kr: "비고", en: "Etc"}},
+		{name:{kr: "수정자", en: "Changed by"}},
+		{name:{kr: "수정일", en: "Changed on"}},
+	]
 }
 
 var message = {
