59f81153adf914e6d8f78555eba175a3d159f432
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-03-08 18:19:05 +0900

    Update Autolabeller_Production_Result.vue

--- src/views/Autolabeller_Production_Result.vue
+++ src/views/Autolabeller_Production_Result.vue
@@ -5,11 +5,11 @@
     <!-- 박스바코드 스캔 화면으로 이동 -->
     <popupautolabeller v-if="popupautoisopen"
       :strPO="req_param.txtPO"
-      :date_hdf="date_hdf"
-      :date_fmfm="date_fmfm"
-      :date_prod="date_prod"
+      :date_hdf="date_hdf.value"
+      :date_fmfm="date_fmfm.value"
+      :date_prod="date_prod.value"
       :strBox="req_param.txtBox"
-      :strLotno="req_param.txtLotno"
+      :strLotno="req_param.cboShift+req_param.txtLotno"
       @autocloseClick="autocloseClick">
     </popupautolabeller>
 
@@ -22,6 +22,7 @@
           id="txtPO"
           ref="txtPO"
           @keyup.enter='displayClick'
+          @click='POClick'
           @focus='fn_SelectAll'
           data-ref="InputContent" inputmode="numeric"
           v-model="req_param.txtPO"
@@ -208,10 +209,10 @@
           :style="{width:'100px', display:'inline-block', 'text-align':'right'}"
         >Lot No</span>
         <select class="form-select btn-sm" aria-label="Default select example"
-          id="cboshift"
-          ref="cboshift"
+          id="cboShift"
+          ref="cboShift"
           @change ='keyupenter'
-          v-model="shift">
+          v-model="req_param.cboShift">
           <option disabled value="">생산조를 선택하세요</option>
           <option v-for="(d, i) in options" :key="i" :value="d.id">{{ d.id }}</option>
         </select>
@@ -254,6 +255,7 @@
   import { reactive, ref, onBeforeMount, onMounted, onUnmounted } from 'vue'
   import { useStore } from 'vuex';
   // import { getdata, formatDate } from '@/helper/filter.js';
+  import { formatDate } from '@/helper/filter.js';
   import { PlaySound } from '@/helper/util.js';
   import popupautolabeller from '@/views/Autolabeller_Production_Result_Scan.vue';
 
@@ -276,8 +278,6 @@
       let recvData = reactive([]);
       let lblMaktx = ref(null);
       let lblProcQty = ref(null);
-      let shift = ref(null);
-      var Validated = true;
 
 			//달력
 			let isDark = ref(false);
@@ -290,10 +290,11 @@
       //focus 이동을 위한 변수
       let txtPO = ref(null);
       let txtBox = ref(null);
-      // let txtLotno = ref(null);
+      let txtLotno = ref(null);
+      let cboShift = ref(null);
 
       //데이터 바인딩
-      let req_param = reactive({txtPO:"", txtBox:"", txtLotno:""});
+      let req_param = reactive({txtPO:"001000775432", txtBox:"", txtLotno:"", cboShift:""});
       let msg = ref(null);
       let msg_color = ref(null);
 
@@ -333,6 +334,16 @@
         options.push({id:"D", name:"D조"});
       }
 
+      function POClick(){
+        // console.log(req_param.txtScan);
+        var tmpPO = document.getElementById("txtPO");
+        tmpPO.setAttribute('inputmode','numeric');
+        console.log(tmpPO.inputMode);
+
+        // scan.value.focus();
+      }
+
+
       function displayClick(){
         // store.commit('loading/startLoading');
         console.log("Production Order : ", req_param.txtPO);
@@ -354,9 +365,6 @@
       function fn_POSearch(){
         // store.commit('loading/startLoading');
         let urlPost = url.value + '/dw/autolabeller/pda/search';
-        // console.log("[req_param]", req_param);
-				// clear data
-				// recvData.splice(0, recvData.length);
 
         //전송 파라미터 : 프로시저 파라미터와 동일하게 구성
         $axios.post(urlPost, {
@@ -377,7 +385,6 @@
               msg.value = "OK";
               lblMaktx.value = res.data[0].maktx;
               lblProcQty.value = res.data[0].procqty.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");  //천단위 콤마
-              Validated = true;
               txtBox.value.focus();
               PlaySound("OK");
             }
@@ -396,12 +403,24 @@
       }
 
       function continueClick(){
-        console.log("continue button click");
-        // if (lblMaktx=""){
-
-        // }
-
-        popupautoisopen.value = true;
+        console.log("txtPO/lblMaktx/txtBox/txtLotno/cboShift", req_param.txtPO, lblMaktx.value, req_param.txtBox, req_param.txtLotno, req_param.cboShift, req_param.txtLotno.length);
+        console.log("date_hdf/date_fmfm/date_prod", formatDate(date_hdf.value, 'YYYYMMDD'), formatDate(date_fmfm.value, 'YYYYMMDD'), formatDate(date_prod.value, 'YYYYMMDD'))
+        if (lblMaktx.value == "" | lblMaktx.value == null){
+          alert("Please input P/O number first.");
+          txtPO.value.focus();
+        // } else if (req_param.txtBox == "" | req_param.txtBox == null){
+        } else if (req_param.txtBox == ""){
+          alert("Please input Box count.");
+          txtBox.value.focus();
+        } else if (req_param.txtLotno == "" | req_param.txtLotno.length != 3){ // | req_param.txtLotno == null){
+          alert("Please input 3 digit Lot No.");
+          txtLotno.value.focus();
+        } else if (req_param.cboShift == ""){ // | req_param.cboShift == null){
+          alert("Please select shift.");
+          cboShift.value.focus();
+        } else{
+          popupautoisopen.value = true;
+        }
       }
 
       function autocloseClick(){
@@ -409,6 +428,23 @@
 
         //처리결과를 반영하기 위해 오더를 다시 조회.
         // fn_POSearch();
+      }
+
+      function clearClick(){
+        req_param.txtPO = "";
+        lblMaktx.value = null;  //null로 처리해야 txtPO 입력필드 disable이 enable로 변경된다.
+        lblProcQty.value = "";
+        req_param.txtBox = "";
+        req_param.txtLotno = "";
+        req_param.cboShift="";
+        msg.value = "";
+
+        date_hdf.value = new Date();
+        date_fmfm.value = new Date();
+        date_prod.value = new Date();
+          
+        // txtPO = ref(null);
+        txtPO.value.focus();  //Question : focus가 작동하지 않는다.
       }
 
       function closeClick(){
@@ -421,14 +457,10 @@
       }
 
       function keyupenter(e){
-        if (e.target.id == "txtPO"){
-          // console.log(req_param.scan);
-          var tmpPO = document.getElementById("txtPO");
-          tmpPO.setAttribute('inputmode','none');
-          console.log(tmpPO.inputMode);
-
-          //API전송
-          // fn_SendAPI();
+        if (e.target.id == "txtBox"){
+          txtLotno.value.focus();
+        } else if (e.target.id == "txtLotno"){
+          cboShift.value.focus();
         }
       }
 
@@ -436,10 +468,9 @@
       return {
         window_width,
         window_height,
-        Validated,
         lblMaktx,
         lblProcQty,
-        shift,
+        cboShift,
 				isDark,
 				isRange,
 				date_hdf,
@@ -447,6 +478,7 @@
         date_prod,
         txtPO,
         txtBox,
+        txtLotno,
         req_param,
         msg,
         msg_color,
@@ -458,24 +490,15 @@
         popupautoisopen,
         autocloseClick,
         displayClick,
+        POClick,
         continueClick,
+        clearClick,
         closeClick,
         fn_SelectAll,
         keyupenter,
       };
     },
 
-    // computed: {
-    //   clickable(e) {
-    //     console.log("isTrue: ", e);
-    //     if (e == false){
-    //       return false;
-    //     }
-    //     else {
-    //       return true;
-    //     }
-    //   }
-    // }    
   }
 </script>
 <style lang="scss">
