9f7951a7385e0b676058741159f7d1a88c9c5a42
Yunhc <yunhc@wavelink.co.kr>
2022-03-10 10:39:56 +0900

    Update FG_Receipt.vue

--- src/views/FG_Receipt.vue
+++ src/views/FG_Receipt.vue
@@ -424,10 +424,4 @@
   }
 </script>
 <style lang="scss">
-  @import "~ag-grid-community/dist/styles/ag-grid.css";
-  @import "~ag-grid-community/dist/styles/ag-theme-alpine.css";
-
-  .ag-header-cell-label {
-    justify-content: center;
-  }
 </style>
