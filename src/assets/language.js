5ea857559e6a04e75dbd8719dae56cccb4261a9d
Yunhc <96555481+Yunhc@users.noreply.github.com>
2022-03-28 18:11:52 +0900

    Update language.js

--- src/assets/language.js
+++ src/assets/language.js
@@ -101,6 +101,48 @@ var master_material = {
 	]
 }
 
+var quality_scrap = {
+	titleBarNo: {kr: "바코드", en: "Barcode"},
+	titleStatus:{kr: "재고상태", en: "Status"},
+	titleMatnr:{kr: "자재코드", en: "Material"},
+	lblMaktx:{kr: "자재 상세내역을 표시합니다.", en: "Display the material description."},
+	titleQty:{kr: "수량", en: "Quantity"},
+	lblSend:{kr: "전송", en: "Send"},
+	lblDelete:{kr: "삭제", en: "Delete"},
+	lblClear:{kr: "초기화", en: "Clear"},
+	lblClose:{kr: "종료", en: "Close"},
+	grid1:[
+		// {name:{kr: "", en: ""}},
+		{name:{kr: "선택", en: "Sel"}},
+		{name:{kr: "바코드", en: "Barcode"}},
+		{name:{kr: "재고상태", en: "Status"}},
+		{name:{kr: "처리유형", en: "Proc. Type"}},
+		{name:{kr: "라벨유형", en: "Label Type"}},
+		{name:{kr: "수량", en: "Quantity"}},
+		{name:{kr: "단위", en: "Unit"}},
+		{name:{kr: "자재코드", en: "Material"}},
+		{name:{kr: "자재명", en: "Material Description"}},
+		{name:{kr: "그룹바코드", en: "Group Barcode"}},
+		{name:{kr: "낱박스존재", en: "Box Barcode Existence"}},
+		{name:{kr: "플랜트", en: "Plant"}},
+		{name:{kr: "작업장", en: "Workcenter"}},		
+		{name:{kr: "저장위치", en: "Stor. Loc"}},
+		{name:{kr: "S/O 번호", en: "S/O No"}},
+		{name:{kr: "S/O 품번", en: "S/O Item"}},
+		{name:{kr: "이전바코드", en: "Pre Barcode"}},
+		{name:{kr: "이전자재코드", en: "Pre Material"}},
+		{name:{kr: "D/O 번호", en: "D/O No"}},
+		{name:{kr: "D/O 품번", en: "D/O Item"}},
+		{name:{kr: "Lot 번호", en: "Lot No"}},
+		{name:{kr: "생산일자", en: "Prod. Date"}},
+		{name:{kr: "LPM전면(내)", en: "LPM_FO"}},
+		{name:{kr: "LPM전면(외)", en: "LPM_FI"}},
+		{name:{kr: "LPM후면(내)", en: "LPM_BO"}},
+		{name:{kr: "LPM후면(외)", en: "LPM_BI"}},
+		{name:{kr: "경면판(전)", en: "Plate_F"}},
+		{name:{kr: "경면판(후)", en: "Plate_B"}},
+	]
+}
 
 var message = {
 	lblNotice:{kr: "알림", en: "Notice"},
@@ -115,4 +157,5 @@ export default {
 	usermanagement,
 	master_company,
 	master_material,
+	quality_scrap,
 }
